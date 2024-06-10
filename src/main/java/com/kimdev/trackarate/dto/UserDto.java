5b808b622e01d1fd1f37e3ded14d5f85e6890e69
Kim Robert <kimrobert.nb@gmail.com>
2024-06-10 17:16:38 +0200

    add many to many mapping to userdto

--- src/main/java/com/kimdev/trackarate/dto/UserDto.java
+++ src/main/java/com/kimdev/trackarate/dto/UserDto.java
@@ -1,5 +1,9 @@
 package com.kimdev.trackarate.dto;
 
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 import com.kimdev.trackarate.models.User;
 
 import lombok.AllArgsConstructor;
@@ -21,22 +25,70 @@ public class UserDto {
 
     private String password;
 
+    private List<UUID> senseiListIds;
+
+    private List<UUID> studentsListIds;
+
+    private List<UUID> contactsListIds;
+
     public static UserDto fromEntity(User user) {
         return UserDto.builder()
                 .username(user.getUsername())
                 .email(user.getEmail())
                 .phone(user.getPhone())
                 .password(user.getPassword())
+                .senseiListIds(
+                        user.getSenseiList()
+                                .stream()
+                                .map(User::getId)
+                                .collect(Collectors.toList()))
+                .studentsListIds(
+                        user.getStudentsList()
+                                .stream()
+                                .map(User::getId)
+                                .collect(Collectors.toList()))
+                .contactsListIds(
+                        user.getContactsList()
+                                .stream()
+                                .map(User::getId)
+                                .collect(Collectors.toList()))
                 .build();
     }
 
     public static User toEntity(UserDto userDto) {
-        return User.builder()
+        User user = User.builder()
                 .username(userDto.getUsername())
                 .email(userDto.getEmail())
                 .phone(userDto.getPhone())
                 .password(userDto.getPassword())
                 .build();
+
+        List<User> senseiList = userDto.getSenseiListIds()
+                .stream()
+                .map(id -> User.builder()
+                        .id(id)
+                        .build())
+                .collect(Collectors.toList());
+
+        List<User> studentsList = userDto.getStudentsListIds()
+                .stream()
+                .map(id -> User.builder()
+                        .id(id)
+                        .build())
+                .collect(Collectors.toList());
+
+        List<User> contactsList = userDto.getContactsListIds()
+                .stream()
+                .map(id -> User.builder()
+                        .id(id)
+                        .build())
+                .collect(Collectors.toList());
+
+        user.setSenseiList(senseiList);
+        user.setStudentsList(studentsList);
+        user.setContactsList(contactsList);
+
+        return user;
     }
 
 }
